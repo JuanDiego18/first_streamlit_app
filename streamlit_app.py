86c7a711548f4e4d95200e767ebcf54df69f0948
Juan Diego Vargas Rosales <97452574+JuanDiego18@users.noreply.github.com>
2022-08-15 22:26:06 -0600

    Update streamlit_app.py
    Displays the Fruityvice recommendation as a Dataframe in Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,4 +21,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # Just displays it in json format
+
+# This lines converts the json format into a dataframe 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# This lines displays the dataframe
+streamlit.dataframe(fruityvice_normalized)
