47ac30f0af482fc570b519a00e36f4d2608c0994
Juan Diego Vargas Rosales <97452574+JuanDiego18@users.noreply.github.com>
2022-08-15 22:29:56 -0600

    Update streamlit_app.py
    Deleted the raw json format and separated the fruit variable from the URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # Just displays it in json format
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+
 
 # This lines converts the json format into a dataframe 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
